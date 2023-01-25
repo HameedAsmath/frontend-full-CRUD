a68f2dc476175bb1114906d3ed8407b52eb60e35
[Hameed Asmath] <75921618+HameedAsmath@users.noreply.github.com>
2023-01-25 22:38:42 +0530

    Initial Commit

---
+++ src/components/UserList.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function UseList() {
+  const [userData, setUserData] = useState("");
+  const getAllUsers = async () => {
+    const res = await axios.get("/getUsers");
+    if(res.data.users.length>0){
+      setUserData(res.data.users)
+    }
+  };
+  useEffect(() => {
+    getAllUsers();
+  }, [userData]);
+
+  //Edit
+  const handleEdit = async(userId)=>{
+    const name = prompt("Enter your name")
+    const email = prompt("Enter your email")
+    if(!name || !email){
+      alert("Name and Email is Required")
+    }
+    else{
+      const res = await axios.put(`/editUser/${userId}`,{
+        name,
+        email
+      })
+      console.log(res)
+    }
+  }
+  const handleDelete = async(userId)=>{
+    const res = await axios.delete(`/deleteUser/${userId}`)
+    console.log(res)
+  }
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-8">
+            <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">
+              All Users
+            </h1>
+          </div>
+          <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+            <table className="table-auto w-full text-left whitespace-no-wrap">
+              <thead>
+                <tr>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                    Name
+                  </th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    Email
+                  </th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    Edit
+                  </th>
+                  <th className="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                    Delete
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {userData &&
+                  userData.map((user) => (
+                    <tr>
+                      <td className="px-4 py-3">{user.name}</td>
+                      <td className="px-4 py-3">{user.email}</td>
+                      <td className="px-4 py-3">
+                        <button className="hover:text-green-500" onClick={()=> handleEdit(user._id)}>Edit</button>
+                      </td>
+                      <td className="px-4 py-3 text-lg text-gray-900">
+                        <button className="hover:text-red-500" onClick={()=>handleDelete(user._id)}>Delete</button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default UseList;
